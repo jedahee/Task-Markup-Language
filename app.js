e3ba8b0d761102b509f16bf6501013313f81c9ac
jedahee02 <jedahee02@gmail.com>
2021-02-01 21:41:24 +0100

    finalizado header pag principal

---
+++ app.js
@@ -0,0 +1,19 @@
+var boton_menu = document.getElementById("boton_menu");
+var img_menu = document.getElementById("img_menu");
+var nav = document.getElementById("nav");
+var activo = true;
+
+boton_menu.addEventListener("click", () => {
+    boton_menu.classList.toggle('hacerAnimacion');
+    
+    //toggle style
+    if (activo) {
+        img_menu.style.opacity = 0;
+        nav.style.transform = 'translateY(0%)';
+        activo = false;
+    } else {
+        img_menu.style.opacity = 1;
+        nav.style.transform = 'translateY(-100%)';
+        activo = true;
+    }
+});
